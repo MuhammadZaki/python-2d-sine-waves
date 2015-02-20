3019a7395d4b1d803872b291b6eb4a97cc1e2696
Ian <mcloughlin.ian@gmail.com>
2015-02-20 17:43:16 +0000

    Added fill

--- sine-waves.py
+++ sine-waves.py
@@ -5,6 +5,9 @@ import numpy as np
 
 x = np.arange(-8 * np.pi, 8 * np.pi, 0.01)
 y = np.sin(x)
+zeros = np.zeros(x.size)
 
 pl.plot(x, y, "b-")
+pl.fill_between(x, zeros, y, facecolor='blue', alpha=0.8)
+
 pl.show()
